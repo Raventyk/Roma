468b04193e2baceec66e69b63ffd43896715348c
Raventyk <tra@nsaero.ru>
2025-11-14 00:32:22 +0500

    Improve input validation in voting eligibility check
    Added error handling for non-integer age input and stripped whitespace from user inputs.

--- the ability to vote.py
+++ the ability to vote.py
@@ -1,4 +1,10 @@
 def can_vote(age, citezen, out_human):
+    try:
+        age = int(age)
+    except ValueError:
+        print("Вы ввели не числовые значения ")
+        return False
+
     if age < 18:
         return False
     if citezen == "нет":
@@ -9,9 +15,9 @@ def can_vote(age, citezen, out_human):
 
 
 def data():
-    age = int(input("Введите Ваш возраст "))
-    citezen = input("Вы гражданин страны ? (Введите 'Да' или 'Нет') ").lower()
-    out_human = input("Есть ли у вас судимость или запрет на голосование? (Введите 'Да' или 'Нет') ").lower()
+    age = (input("Введите Ваш возраст "))
+    citezen = input("Вы гражданин страны ? (Введите 'Да' или 'Нет') ").lower().strip()
+    out_human = input("Есть ли у вас судимость или запрет на голосование? (Введите 'Да' или 'Нет') ").lower().strip()
     return age, citezen, out_human
 
 
