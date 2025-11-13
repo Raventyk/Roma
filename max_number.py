374632faba836531e8c8727f5bc0af454caf9eff
Raventyk <tra@nsaero.ru>
2025-11-13 22:59:02 +0500

    Update max_number.py

--- max_number.py
+++ max_number.py
@@ -1,31 +1,12 @@
-#def max_number(a, b):
-    #if a > b:
-        #return a
-    #else:
-        #return b
-#def max_number(a, b):
-    #return {True: a, False: b}[a >= b]
 def max_number(a, b):
     return (a + b + abs(a - b)) / 2  # математически , если без if и булевых подходов.
 
 
 def test_max_number():
-    assert max_number(1, 2) == 2
-    assert max_number(17, 4) == 17
-    assert max_number(-5, 6) == 6
-    assert max_number(180, -4) == 180
+    assert max_number(32, -522) == 32
+    assert max_number(-180, 44) == 44
     assert max_number(5,5) == 5
-    assert max_number(-7,-7) == -7
-    assert max_number(0,0) == 0
     print("Все тесты пройдены !")
-
-
-test_max_number()
-
-num1 = 17
-num2 = 16
-
-print(max_number(num1, num2))
 
 
 def empty_function():
@@ -37,5 +18,11 @@ def even_numbers(n):
         yield num
 
 
+test_max_number()
+
 for result in even_numbers(8):
     print(result)
+
+num1 = 17
+num2 = 16
+print(max_number(num1, num2))
