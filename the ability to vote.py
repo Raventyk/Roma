7903b3584b93233fcb0c0f3ab954914d8a3c4ee4
Raventyk <tra@nsaero.ru>
2025-11-14 19:18:58 +0500

    Update the ability to vote.py

--- the ability to vote.py
+++ the ability to vote.py
@@ -1,10 +1,4 @@
 def can_vote(age, citezen, out_human):
-    try:
-        age = int(age)
-    except ValueError:
-        print("Вы ввели не числовые значения ")
-        return False
-
     if age < 18:
         return False
     if citezen == "нет":
@@ -14,19 +8,27 @@ def can_vote(age, citezen, out_human):
     return True
 
 
-def data():
-    age = (input("Введите Ваш возраст "))
+def input_data():
+    input_age = input("Введите Ваш возраст ")
+
+    try:
+        age = int(input_age)
+    except ValueError:
+        print("Ошибка : Вы ввели не числовое значение ")
+        age = 0
+
     citezen = input("Вы гражданин страны ? (Введите 'Да' или 'Нет') ").lower().strip()
     out_human = input("Есть ли у вас судимость или запрет на голосование? (Введите 'Да' или 'Нет') ").lower().strip()
     return age, citezen, out_human
 
 
 def main():
-    age, citezen, out_human = data()
+    age, citezen, out_human = input_data()
 
     if can_vote(age, citezen, out_human):
         print("Голосуйте!")
     else:
         print("Вам нельзя голосовать!")
+
 
 main()
